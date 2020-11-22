11dd1e41e7f6cd00d933564b4fd0fa004e1f4ac2
Tad <tad@spotco.us>
2020-11-22 02:53:07 -0500

    Very simple bot challenger

--- sbnr/config.php
+++ sbnr/config.php
@@ -30,7 +30,7 @@ $SBNR_SHADOW_INSTANCE = "https://shadow.example.tld";
 $SBNR_SHADOW_LANDSCAPE = "SNBR_TEST";
 
 //Security
-$SBNR_CAPTCHA_WALL = false && extension_loaded('gd');
+$SBNR_CAPTCHA_WALL = false;
 $SBNR_CAPTCHA_WALL_PAGES = array();
 $SBNR_SEC_HTTPS_ONLY = false; //Enable if using HTTPS
 $SBNR_SEC_CLIENT_HARDENING = true; //Disable if globally added by webserver
